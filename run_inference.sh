a80d0644cc6b45a2ed86cfbe189be1ddd4ca4776
edhendo <edhendo@btinternet.com>
2022-02-14 16:07:52 +0000

    Final step

--- run_inference.sh
+++ run_inference.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-python3 inference.py --model_dir "/home/ed/AutoGAF/models/fold2" \
-                     --use_attention "False" \
-                     --in_image_dir "/home/ed/AutoGAF/data/CTs" \
-                     --out_image_dir "/home/ed/AutoGAF/data/test_cropped_cts" \
-                     --inference_mode "subvolumes" \
-                     --Locator_image_resolution 64 128 128 \
-                     --cropped_image_resolution 64 128 128
+python3 AutoGAF/inference.py --model_dir "/home/ed/AutoGAF/models/fold2" \
+                             --use_attention "False" \
+                             --in_image_dir "/home/ed/AutoGAF/data/CTs" \
+                             --out_image_dir "/home/ed/AutoGAF/data/test_cropped_cts" \
+                             --inference_mode "subvolumes" \
+                             --Locator_image_resolution 64 128 128 \
+                             --cropped_image_resolution 64 128 128
--- run_training.sh
+++ run_training.sh
@@ -2,12 +2,12 @@
 folds='1 2 3 4 5'
 for fold_num in $folds
 do
-    python3 training.py --fold_num $fold_num \
-                        --model_dir "/home/ed/AutoGAF/models/attention" \
-                        --image_dir "/home/ed/AutoGAF/data/rescaled_CTs" \
-                        --CoM_targets_dir "/home/ed/AutoGAF/data" \
-                        --train_BS 8 \
-                        --val_BS 8 \
-                        --train_workers 4 \
-                        --val_workers 4
+    python3 AutoGAF/train.py --fold_num $fold_num \
+                             --model_dir "/home/ed/AutoGAF/models/attention" \
+                             --image_dir "/home/ed/AutoGAF/data/rescaled_CTs" \
+                             --CoM_targets_dir "/home/ed/AutoGAF/data" \
+                             --train_BS 8 \
+                             --val_BS 8 \
+                             --train_workers 4 \
+                             --val_workers 4
 done
