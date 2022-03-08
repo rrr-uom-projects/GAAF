abd63e2e698826d9b5826ee2b8acb8e4e45a5a63
edhendo <edhendo@btinternet.com>
2022-03-08 11:15:30 +0000

    keep consistent naming

--- tests/test_inference.py
+++ tests/test_inference.py
@@ -1,5 +1,5 @@
 import pytest
-from ..GAAF.inference_module import Locator_inference_module, setup_argparse
+from ..GAAF.inference_module import Locator_inference_module
 
 def test_check_resolutions():
     # create class instance
@@ -7,15 +7,15 @@ def test_check_resolutions():
     # test too many dims specified
     Locator_image_resolution = [64,256,256,1]
     cropped_image_resolution = [64,256,256]
-    inf_module.Locator_resolution = tuple([int(res) for res in Locator_image_resolution])
-    inf_module.output_resolution = tuple([int(res) for res in cropped_image_resolution])
+    inf_module.Locator_image_resolution = tuple([int(res) for res in Locator_image_resolution])
+    inf_module.output_image_resolution = tuple([int(res) for res in cropped_image_resolution])
     with pytest.raises(ValueError):
         inf_module._check_resolutions()
     # test too few dims specified
     Locator_image_resolution = [64,256,256]
     cropped_image_resolution = [64,256]
-    inf_module.Locator_resolution = tuple([int(res) for res in Locator_image_resolution])
-    inf_module.output_resolution = tuple([int(res) for res in cropped_image_resolution])
+    inf_module.Locator_image_resolution = tuple([int(res) for res in Locator_image_resolution])
+    inf_module.output_image_resolution = tuple([int(res) for res in cropped_image_resolution])
     with pytest.raises(ValueError):
         inf_module._check_resolutions()
 
