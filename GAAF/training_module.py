651dfcf56ab09f491c3dc39ea542dcedfb358cd3
edhendo <edhendo@btinternet.com>
2022-08-29 15:34:32 +0100

    add testing capability

--- GAAF/training_module.py
+++ GAAF/training_module.py
@@ -6,14 +6,17 @@ from torch.optim.lr_scheduler import ReduceLROnPlateau
 import torch.utils.data as data
 from torch.utils.data import DataLoader
 import numpy as np
+from scipy.optimize import curve_fit
+from scipy.stats import norm
 import os
 from os.path import join
 import argparse as ap
 import pickle
+from tqdm import tqdm
 
 from model import Locator, Attention_Locator
 from trainers import Locator_trainer
-from dataset import Locator_Dataset
+from dataset import Locator_Dataset, Locator_Testset
 from utils import *
 
 class train_locator():
@@ -75,6 +78,91 @@ class train_locator():
         self.trainer.fit()
         # Romeo Dunn
 
+class test_locator():
+    def __init__(self, args):
+        # make the results directory
+        self.res_dir = join(args.model_dir, "results/") 
+        try_mkdir(self.res_dir)
+        self.res_fname = join(self.res_dir, f"fold{str(args.fold_num)}.npy")
+        self.checkpoint_dir = join(args.model_dir, "fold"+str(args.fold_num))
+        
+        # Create main logger
+        self.logger = get_logger('Locator_Testing')
+
+        # Create the model
+        if args.use_attention:
+            self.model = Attention_Locator(n_targets=1, in_channels=1)
+        else:
+            self.model = Locator(filter_factor=1, n_targets=1, in_channels=1)
+        for param in self.model.parameters():
+            param.requires_grad = False
+
+        # load the weights from the best checkpoint
+        self.model.load_best(self.checkpoint_dir, self.logger)
+
+        # put the model on GPU(s)
+        self.device='cuda'
+        self.model.to(self.device)
+
+        # allocate ims to train, val and test
+        dataset_size = len(getFiles(args.image_dir))
+        _, _, test_inds = k_fold_split_train_val_test(dataset_size, fold_num=args.fold_num, seed=args.seed)
+
+        # load in the CoM targets
+        with open(join(args.CoM_targets_dir, "CoM_targets.pkl"), 'rb') as f:
+            CoM_targets = pickle.load(f)
+
+        # load in the spacings
+        with open(join(args.CoM_targets_dir, "spacings.pkl"), 'rb') as f:
+            self.spacings = pickle.load(f)
+
+        # Create them dataloaders
+        test_data = Locator_Testset(imagedir=args.image_dir, CoM_targets=CoM_targets, image_inds=test_inds)
+        self.test_loader = DataLoader(dataset=test_data, batch_size=1, shuffle=False, pin_memory=False, num_workers=0, worker_init_fn=lambda _: np.random.seed(int(torch.initial_seed())%(2**32-1)))
+    
+    def run_testing(self):
+        # setup results array
+        results = np.zeros((len(self.test_loader), 3))
+        # Start testing
+        for im_idx, sample in enumerate(tqdm(self.test_loader)):
+            ct_im = sample['ct_im'].type(torch.FloatTensor)
+            target = sample['target'].numpy()[0]
+            h_target = sample['h_target'].type(torch.FloatTensor)
+            fname = sample['fname'][0]
+            # send tensors to GPU
+            ct_im = ct_im.to(self.device)
+            h_target = h_target.to(self.device)
+            # get the model prediction and MSE loss
+            output = self.model(ct_im)
+            loss = torch.nn.MSELoss()(output, h_target)
+            # get the predicted location coordinates
+            pred_heatmap = output.detach().cpu().numpy()[0,0]
+            im_shape = pred_heatmap.shape
+            argmax_pred = np.unravel_index(np.argmax(pred_heatmap), im_shape)
+            t = np.indices(im_shape).astype(np.float)
+            # define 3D gauss function
+            def prod_3d_gaussian(t, mu_i, mu_j, mu_k):
+                pos = np.array([mu_i, mu_j, mu_k])
+                t = t.reshape((3,) + im_shape)
+                dist_map = np.sqrt(np.sum([np.power((2*(t[0] - pos[0])), 2), np.power((t[1] - pos[1]), 2), np.power((t[2] - pos[2]), 2)], axis=0))
+                gaussian = np.array(norm(scale=10).pdf(dist_map), dtype=np.float)
+                return gaussian.ravel()
+            # get the predicted location coordinates
+            p_opt, _ = curve_fit(prod_3d_gaussian, t.ravel(), pred_heatmap.ravel(), p0=argmax_pred)
+            # find euclidean distance
+            spacing = self.spacings[fname.replace('.npy','.nii')][1]
+            target *= spacing
+            argmax_pred *= spacing
+            p_opt *= spacing
+            argmax_res = np.sqrt(np.power(target[0] - argmax_pred[0], 2) + np.power(target[1] - argmax_pred[1], 2) + np.power(target[2] - argmax_pred[2], 2))
+            fitted_res = np.sqrt(np.power(target[0] - p_opt[0], 2) + np.power(target[1] - p_opt[1], 2) + np.power(target[2] - p_opt[2], 2))
+            # store results
+            results[im_idx, 0] = loss
+            results[im_idx, 1] = argmax_res
+            results[im_idx, 2] = fitted_res
+        # save results array
+        np.save(self.res_fname, results)
+        # Romeo Dunn
 
 def setup_argparse():
     parser = ap.ArgumentParser(prog="Main training program for 3D location-finding network \"Locator\"")
---
+++ run_testing.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+folds='1 2 3 4 5'
+for fold_num in $folds
+do
+    python3 GAAF/test.py --fold_num $fold_num \
+                         --model_dir "/home/ed/GAAF/models/headHunter2" \
+                         --image_dir "/home/ed/segmentation_work/GAAF_headHunter_train_data_HNSCC/rescaled_CTs" \
+                         --CoM_targets_dir "/home/ed/segmentation_work/GAAF_headHunter_train_data_HNSCC"
+done
