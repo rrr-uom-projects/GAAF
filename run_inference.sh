794d217fd8a4612b46b62f3ed127944faafec626
edhendo <edhendo@btinternet.com>
2022-11-02 15:48:51 +0000

    generalise path names

--- run_inference.sh
+++ run_inference.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-python3 -m GAAF.inference   --model_dir "/home/ed/GAAF/models/headHunter2/fold1/" \
+python3 -m GAAF.inference   --model_dir "/path/to/directory/where/model/has/been/saved" \
                             --use_attention "True" \
-                            --in_image_dir "/mnt/driger/hn_data/Aza_HnN/CTs" \
-                            --out_image_dir "/mnt/driger/hn_data/Aza_HnN/test/CTs" \
-                            --out_CoMs_dir "None"\
-                            --in_mask_dir "/mnt/driger/hn_data/Aza_HnN/Structures" \
-                            --out_mask_dir "/mnt/driger/hn_data/Aza_HnN/test/Structures" \
+                            --in_image_dir "/path/to/directory/containing/nifti/CTs" \
+                            --out_image_dir "/path/to/directory/where/processed/CTs/will/be/saved" \
+                            --out_CoMs_dir "/path/to/directory/where/CoM_predictions/file/will/be/saved" \
+                            --in_mask_dir "/path/to/directory/containing/nifti/masks/of/the/Structures" \
+                            --out_mask_dir "/path/to/directory/where/processed/Structures/will/be/saved" \
                             --Locator_image_resolution 64 128 128 \
                             --cropped_image_resolution 64 200 200 \
                             --cropped_image_slice_thickness 3.0
--- run_training.sh
+++ run_training.sh
@@ -3,9 +3,9 @@ folds='1 2 3 4 5'
 for fold_num in $folds
 do
     python3 -m GAAF.train   --fold_num $fold_num \
-                            --model_dir "/home/ed/GAAF/models/dummy" \
-                            --image_dir "/home/ed/segmentation_work/GAAF_headHunter_train_data_HNSCC/rescaled_CTs" \
-                            --CoM_targets_dir "/home/ed/segmentation_work/GAAF_headHunter_train_data_HNSCC" \
+                            --model_dir "/path/to/directory/where/model/will/be/saved" \
+                            --image_dir "/path/to/directory/where/processed/CTs/have/been/saved" \
+                            --CoM_targets_dir "/path/to/directory/where/CoM_targets/file/has/been/saved" \
                             --train_BS 8 \
                             --val_BS 8 \
                             --train_workers 4 \
