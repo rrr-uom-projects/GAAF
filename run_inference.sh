d3309c09f92c0304e5acf485c183c5090c033718
edhendo <edhendo@btinternet.com>
2022-11-21 15:09:19 +0000

    refactor inf. module for clearer output choice

--- run_inference.sh
+++ run_inference.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 python3 -m GAAF.inference   --model_dir "/path/to/directory/where/model/has/been/saved" \
                             --use_attention "True" \
-                            --in_image_dir "/path/to/directory/containing/nifti/CTs" \
-                            --out_image_dir "/path/to/directory/where/processed/CTs/will/be/saved" \
-                            --out_CoMs_dir "/path/to/directory/where/CoM_predictions/file/will/be/saved" \
-                            --in_mask_dir "/path/to/directory/containing/nifti/masks/of/the/Structures" \
-                            --out_mask_dir "/path/to/directory/where/processed/Structures/will/be/saved" \
                             --Locator_image_resolution 64 128 128 \
+                            --in_image_dir "/path/to/directory/containing/nifti/CTs" \
+                            --in_mask_dir "/path/to/directory/containing/nifti/masks/of/the/Structures" \
+                            --output_crop True \
+                            --out_image_dir "/path/to/directory/where/processed/CTs/will/be/saved" \
+                            --out_mask_dir "/path/to/directory/where/processed/Structures/will/be/saved" \
                             --cropped_image_resolution 64 200 200 \
+                            --output_coords True \
+                            --out_CoMs_dir "/path/to/directory/where/CoM_predictions/file/will/be/saved" \
                             --cropped_image_slice_thickness 3.0
